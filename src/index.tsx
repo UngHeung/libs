fe914f4953c11968275694a274b5fbc6a5aa72dd
ungHeung <t01047377477@gmail.com>
2023-10-25 14:25:19 +0900

    feat : 레이아웃, 라우터 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Layout from "./layouts/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Layout>
+        <App />
+      </Layout>
+    </BrowserRouter>
   </React.StrictMode>
 );
