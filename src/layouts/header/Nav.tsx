1315a89cccbab3a8ae68dd97d71fc9e168efc026
ungHeung <t01047377477@gmail.com>
2023-10-25 14:24:19 +0900

    feat : 레이아웃 추가

---
+++ src/layouts/header/Nav.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to={"/"}>홈</Link>
+          <Link to={"/"}>홈</Link>
+          <Link to={"/"}>홈</Link>
+          <Link to={"/"}>홈</Link>
+          <Link to={"/"}>홈</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
