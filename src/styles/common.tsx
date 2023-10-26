8cfb5f185a3537e20d7e3d83cb09bcfd24e0810c
ungHeung <t01047377477@gmail.com>
2023-10-26 15:20:01 +0900

    refactor : 스타일컴포넌트 분리, 공통 스타일 추가

---
+++ src/styles/common.tsx
@@ -0,0 +1,22 @@
+import { css } from "styled-components";
+
+export const StyledButton = css`
+  width: 100px;
+  height: 30px;
+  padding: 5px;
+  border: 1px solid #333;
+  border-radius: 5px;
+  text-align: center;
+  line-height: 18px;
+  cursor: pointer;
+`;
+
+export const StyledMiniCircleButton = css`
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-color: white;
+  text-align: center;
+  line-height: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+`;
